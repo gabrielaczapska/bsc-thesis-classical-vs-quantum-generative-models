6ac54f9837989037738e7abe599c5ae6b840452b
gabriela czapska <gabrielacz@gabrielas-MacBook-Pro.local>
2026-04-14 10:06:47 +0200

    Improve comments and fix small running bugs

--- wgan_model.py
+++ wgan_model.py
@@ -24,10 +24,10 @@ def wasserstein_loss(y_true, y_pred):
     return tf.reduce_mean(y_true * y_pred)
 
 
-# load and prepare a dataset
+# load and prepare a dataset (3x3)
 def load_real_samples(n_dimensions=3):
     dataset = get_bars_and_stripes(n_dimensions)
-    # introduce the 3rd dimension to a single sample
+    # introduce the 3rd dimension (channels) to the data samples
     dataset = dataset.reshape(dataset.shape[0], 3, 3, 1)
     # rewrite 0's into (-1's); a suitable format for the tanh decision function
     dataset = dataset * 2.0 - 1.0
@@ -35,21 +35,18 @@ def load_real_samples(n_dimensions=3):
 
 
 # create a random mini-batch with labels
-def generate_real_samples(dataset, n_samples):
+def select_real_samples(dataset, n_samples):
     # bootstrapping - draw n_samples with replacement
     ix = randint(0, dataset.shape[0], n_samples)
     # X- matrix of n chosen samples with indexes corresponding to ix
     X = dataset[ix]
-    # label real samples with (-1's) - suited for the loss function
+    # label real samples as (-1's) (Wasserstein loss convention)
     y = -ones((n_samples,1))
     return X, y
 
 
-# critic/discriminator model
-
-# clip model
+# clip model - restrict weight values within a predefined boundary range [-clip_value, clip_value]
 class ClipConstraint(Constraint):
-    # constraint the weight values within (-clip_value, clip_value)
     def __init__(self, clip_value):
         self.clip_value = clip_value
 
@@ -57,17 +54,14 @@ class ClipConstraint(Constraint):
         return backend.clip(weights, -self.clip_value, self.clip_value)
 
 
-# critic
-# extract features and classify the input
-def define_critic(in_shape=(3,3,1)):
+# critic - extracts features and classifies the input
+def critic(in_shape=(3,3,1)):
     init = RandomNormal(stddev=0.02)
     const = ClipConstraint(0.01)
     model = Sequential()
-    # process an entire input at once with (3,3) kernel resulting in 8 filters only due to low pattern diversity
+    # process an entire input at once with 8 (3x3) kernels due to low pattern diversity
     # toDo: test with padding="valid"
     model.add(Conv2D(8, (3,3), strides=(1,1), padding="same", kernel_initializer=init, kernel_constraint=const, input_shape=in_shape))
-    # BATCH NORMALISATION -- stabilises changes to the output values and loss calculations by making it sensitive
-    # to the changes in the parameters' values [>>???<<]
     model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
     model.add(Conv2D(8, (3,3), strides=(1,1), padding='same', kernel_initializer=init, kernel_constraint=const))
@@ -83,30 +77,31 @@ def define_critic(in_shape=(3,3,1)):
     return model
 
 
-# generator; latent_dim defines the number of random input variables used to generate the output
-def define_generator(latent_dim):
+# generator - generates the output from a random input vector of size latent_dim
+def generator(latent_dim):
     init = RandomNormal(stddev=0.03)
     model = Sequential()
     # n_nodes -> channels * height * width
+    # (represent input as a 1D data structure)
     n_nodes = 8 * 3 * 3
-    # latent_dim defines the number of input variables used to generate the output;
+    # latent_dim defines the number of RANDOM input variables used to generate the output;
     # each pixel depends on all latent_dim
     model.add(Dense(n_nodes, kernel_initializer=init, input_dim=latent_dim))
     model.add(LeakyReLU(alpha=0.2))
+    # restructure the input into a 3d feature map
     model.add(Reshape((3, 3, 8)))
-    # build structured feature maps from latent representations using transposed convolution
+    # introduce spatial dependencies (feature representations) across all neighbouring pixels
     model.add(Conv2DTranspose(8, (3,3), strides=(1,1), padding='same', kernel_initializer=init))
     model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
-    # represent the output as a single feature map combining all features maps into one
+    # represent the output as a single feature map combining all feature maps into a single output (a generated image)
     model.add(Conv2D(1, (3,3), activation='tanh', padding='same', kernel_initializer=init))
     return model
 
 
-# GAN definition
-# generator training phase: train the generator, keep the critic fixed
-def define_gan(generator, critic):
-    # freeze critic's weights but so that only the generator is updates in combined model
+# GAN
+# generator training phase: train the generator, keep the critic fixed (freeze its weights)
+def gan(generator, critic):
     # keep BatchNormalization layers trainable to avoid unstable training
     for layer in critic.layers:
         if not isinstance(layer, BatchNormalization):
@@ -119,9 +114,7 @@ def define_gan(generator, critic):
     return model
 
 
-# generate fake samples
-
-# generate n_samples latent random vectors of dimension latent_dim
+# generate n latent random vectors of dimension latent_dim
 def generate_latent_points(latent_dim, n_samples):
     x_input = randn(latent_dim, n_samples)
     # reshape the matrix so that each row is a separate latent vector
@@ -134,7 +127,7 @@ def generate_fake_samples(generator, latent_dim, n_samples):
     x_input = generate_latent_points(latent_dim, n_samples)
     # generate fake samples from latent vectors
     X = generator.predict(x_input)
-    # set all the fake samples to 1's that aligns with real samples being set to (-1's)
+    # set all the fake samples to 1's - in contrast to real samples labeling [-1's]; (Wasserstein loss convention)
     y = ones((n_samples, 1))
     return X, y
 
@@ -149,10 +142,11 @@ def train(g_model, c_model, gan_model, dataset, latent_dim, n_epochs=500, n_batc
 
     for i in range(n_training_steps):
         c_real_tmp, c_fake_tmp = list(), list()
-        # critic training
+        # critic training (half batch of real and half batch of fake samples)
         for _ in range(n_critic):
             # train on the real datapoints
-            X_real, y_real = generate_real_samples(dataset, half_batch)
+            X_real, y_real = select_real_samples(dataset, half_batch)
+            # update the parameters of c and keep the loss value
             c_loss_real = c_model.train_on_batch(X_real, y_real)
             c_real_tmp.append(c_loss_real)
 
@@ -165,34 +159,17 @@ def train(g_model, c_model, gan_model, dataset, latent_dim, n_epochs=500, n_batc
         c_fake_history.append(mean(c_fake_tmp))
 
         X_gan = generate_latent_points(latent_dim, n_batch)
-        # label generated samples as real so the loss measures how far the critic's score is from real
-        # the larger the divergence from real, the more intense the generator's training
+        # label generated samples as real to penalise or reward the generator
+        # depending on how well it deluded the critic
         y_gan = -ones((n_batch, 1))
         g_loss = gan_model.train_on_batch(X_gan, y_gan)
         g_history.append(g_loss)
 
-        # keep track of the training process and summarize the performance
-        print('&gt;%d, critic_real_loss=%.3f, critic_fake_loss=%.3f generator_loss=%.3f' % (i+1, c_real_history[-1], c_fake_history[-1], g_loss))
-        if (i+1) % 10 == 0:
-            summarize_performance(i, g_model, latent_dim)
+        # keep track of the training process
+        print('iteration:%d/1500, critic_real_loss=%.3f, critic_fake_loss=%.3f generator_loss=%.3f' % (i+1, c_real_history[-1], c_fake_history[-1], g_loss))
 
     # line plots of loss
     plot_history(c_real_history, c_fake_history, g_history)
-
-
-# VISUALISATION
-def summarize_performance(step, g_model, latent_dim, n_samples=100):
-    X, _ = generate_fake_samples(g_model, latent_dim, n_samples)
-    # rescale the pixel values back to [0, 1]
-    X = (X + 1) / 2.0
-    for i in range(n_samples):
-        # create a grid of 100 samples
-        plt.subplot(10, 10, i+1)
-        plt.axis('off')
-        plt.imshow(X[i, :, :, 0], cmap='gray')
-    filename1 = 'plot_%04d.png' % (step+1)
-    plt.savefig(filename1)
-    plt.close()
 
 
 def plot_history(c_real_history, c_fake_history, g_history):
@@ -204,13 +181,14 @@ def plot_history(c_real_history, c_fake_history, g_history):
     plt.close()
 
 
-latent_dim = 2
-critic = define_critic()
-generator = define_generator(latent_dim)
-gan_model = define_gan(generator, critic)
-dataset = load_real_samples(3)
-print(dataset.shape)
-train(generator, critic, gan_model, dataset, latent_dim)
+if __name__ == '__main__':
+    # choice of latent_dim (at least log_2(14)~14)
+    latent_dim = 8
+    critic = critic()
+    generator = generator(latent_dim)
+    gan_model = gan(generator, critic)
+    dataset = load_real_samples(3)
+    train(generator, critic, gan_model, dataset, latent_dim)
 
 
 
